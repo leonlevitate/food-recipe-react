deda1e6ff3e594eb50092d87d0cb4914b780158b
Leon Linton <leon.linton@live.co.uk>
2019-09-02 20:54:36 +0100

    Adds the API keys from Edamam

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const APP_ID = "07786135";
+  const APP_KEY = "f888def4ba987a41cefa7a02fd575289";
+
   return (
     <div className="App">
       <h1>Hello React</h1>
     </div>
   );
-}
+};
 
 export default App;
