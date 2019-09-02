b3eeccdf038d0be5dfa8e8585591adf61f8b0fbd
Leon Linton <leon.linton@live.co.uk>
2019-09-02 21:08:19 +0100

    Adds initial search form

--- src/App.js
+++ src/App.js
@@ -6,9 +6,16 @@ const App = () => {
   const APP_ID = "07786135";
   const APP_KEY = "f888def4ba987a41cefa7a02fd575289";
 
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 };
